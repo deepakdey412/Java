b1bfc4ea7089875d259f06d382e8b9e2a42249c1
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-17 22:11:02 +0530

    Updates

---
+++ java_8/Predefined_Functional_Interface/Supplier/Supplier_Demo.java
@@ -0,0 +1,20 @@
+package Supplier;
+
+import java.util.function.Supplier;
+
+public class Supplier_Demo {
+    public static void main(String[] args) {
+        Supplier<String> supplier = () -> {
+            String otp = "";
+            for (int i = 1; i <= 10; i++) {
+                otp = otp + (int)(Math.random() * 10);
+            }
+            return otp;
+        };
+
+        System.out.println(supplier.get());
+        System.out.println(supplier.get());
+        System.out.println(supplier.get());
+        System.out.println(supplier.get());
+    }
+}
